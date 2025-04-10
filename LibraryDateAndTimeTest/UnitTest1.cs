ce7b6fba520c1fda90c22e99eb0786d7e3684f2e
Nastya <bgtkva2006@gmail.com>
2025-04-10 20:23:36 +0300

    первый тест

---
+++ LibraryDateAndTimeTest/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Library_Date_and_Time;
+
+namespace LibraryDateAndTimeTest
+{
+    public class Tests
+    {
+
+        [Test]
+        public void TestDaysBetween()
+        {
+            DateTime today = new DateTime(2025, 04, 10);
+            DateTime yesterday = new DateTime(2025, 04, 04);
+
+        }
+        
+    }
+}
---
+++ Library_Date_and_Time/DateDateConvert.cs
@@ -0,0 +1,26 @@
+﻿namespace Library_Date_and_Time
+{
+    public class DateDateConvert
+    {
+
+        public static int DaysBetween(DateTime dateTime1, DateTime dateTime2)
+        {
+            // Проверяем на равные даты. Если равны, то разница 0.
+            if (dateTime1 == dateTime2)
+            {
+                return 0;
+            }
+
+            // Проверяем, если dateTime1 позже dateTime2
+            if (dateTime1 > dateTime2)
+            {
+                throw new ArgumentException("dateTime1 не может быть позже dateTime2.");
+            }
+
+            // Используем свойство .Subtract, чтобы получить количество дней между датами
+            TimeSpan difference = dateTime2 - dateTime1;
+            return difference.Days;
+        }
+    }
+
+}
